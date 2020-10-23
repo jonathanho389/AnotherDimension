fa00685ffb84372672a658b788ee4b2a0704179d
Jonathan Ho <hojon389@gmail.com>
2020-10-23 13:23:11 -0400

    largest works

--- ArrayOps.java
+++ ArrayOps.java
@@ -7,4 +7,14 @@ public class ArrayOps{
     }
     return sum;
   }
+  public static int largest(int[] arr){
+    int length = arr.length;
+    int max = 0;
+    for(int i = 0;i < length;i++){
+      if(arr[i] > max){
+        max = arr[i];
+      }
+    }
+    return max;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[] args) {
     int[] newarr = {1,2,3};
-    System.out.print(ArrayOps.sum(newarr));
+    System.out.println(ArrayOps.sum(newarr));
+    System.out.println(ArrayOps.largest(newarr));
   }
 }
